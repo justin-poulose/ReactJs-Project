b68a086b6520af8276fd41ee4345abedc73a972a
justin-poulose <justinchekkan@gmail.com>
2024-01-20 16:58:29 -0500

    created json local server working

--- poem-blog/src/ContentHome.js
+++ poem-blog/src/ContentHome.js
@@ -1,16 +1,17 @@
 import { useState, useEffect } from 'react';
 import PostList from './PostList';
 const Homes = () => {
-    const [posts, setPost] = useState(
-        [
-            {id:1,title:'Whispers of the Breeze',poem:"In twilight's tender grasp they rise<br>The whispers of the breeze<br>Dancing leaves in moonlit skies<br>A secret told with ease.",author:'Emily Rivers'},
-            {id:2,title:'Echoes of Silence',poem:"In stillness, silence softly weaves <br> Echoes linger in tranquil eaves <br> Unspoken words in quiet strife <br> A symphony of muted life.",author:'Liam Serenade'},
-            {id:3,title:'Serenade of the Stars',poem:"Midnight's curtain gently draws <br> Celestial serenade without flaws <br> Stars compose in silent light <br> A cosmic ballad in the night.",author:'Luna Evernight'}
-        ]
-    )
+    const [posts, setPost] = useState(null)
     
    useEffect(() => {
-
+    fetch('http://localhost:8000/posts')
+    .then(res => {
+        return res.json()
+    })
+    .then(data => {
+        setPost(data);
+        console.log(posts);
+    })
    },[]    
    );
 
@@ -20,7 +21,7 @@ const Homes = () => {
                 <h2 style={{color:'#1e37c5'}}>HomeFeed</h2>
                 <input type="textbox" value="search Poem" ></input>
             </div>
-            <PostList posts={posts.filter((posts) => true)}/>
+            {posts && <PostList posts={posts}/>}
         </div>
     );
 }
