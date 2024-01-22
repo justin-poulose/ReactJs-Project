4847769edb06988896070f97badcc4ec49382563
justin-poulose <justinchekkan@gmail.com>
2024-01-22 09:19:15 -0500

    configured search

--- poem-blog/src/ContentHome.js
+++ poem-blog/src/ContentHome.js
@@ -4,18 +4,23 @@ import { useState } from 'react';
 
 const Homes = () => {
     const {data: posts, error} = useFetch("http://localhost:8000/posts");
-    const [searchtext,setsearchtext] = useState('search Verse');
+    const [searchtext,setsearchtext] = useState('');
    
+    const filteredPosts = posts ? posts.filter(post =>
+        post.title.toLowerCase().includes(searchtext.toLowerCase()) ||
+        post.poem.toLowerCase().includes(searchtext.toLowerCase()) ||
+        post.author.toLowerCase().includes(searchtext.toLowerCase())
+    ) : [];
 
     return (  
         <div className="home">
             <div className='headerFeed'>
                 <h2 style={{color:'#1e37c5'}}>Home Feed</h2>
                 <input type="textbox" value={searchtext} 
-                onChange={(e)=> setsearchtext(e.target.value)}></input>
+                onChange={(e)=> setsearchtext(e.target.value)} placeholder='Search Verse'></input>
             </div>
             {error && <div className='error'>{error}</div>}
-            {posts && <PostList posts={posts}/>}
+            {filteredPosts && <PostList posts={filteredPosts} />}
         </div>
     );
 }
